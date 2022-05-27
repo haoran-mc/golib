93b75886ece4ed83aa896a34c2aed799d509bd78
haoran-mc <haoran.mc@outlook.com>
2022-05-27 17:49:12 +0800

    :sparkles:day2 Context Router

--- main.go
+++ main.go
@@ -8,18 +8,22 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		_, _ = fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee!</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=gee
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			_, _ = fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
-
 	err := r.Run(":9999")
 	if err != nil {
-		fmt.Printf("error: %v\n", err)
+		fmt.Printf("error: %v\n", err.Error())
 	}
 }
