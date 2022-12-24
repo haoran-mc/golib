97dca9e5a99a8eb6f89b18770a36e842691be050
haoran <haoran.mc@outlook.com>
2022-12-24 15:11:36 +0800

    go_web day5 middleware

--- go_web/main.go
+++ go_web/main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/haoran-mc/go_pkgs/go_web/gee"
 )
 
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
 func main() {
 	r := gee.New()
-	r.GET("/index", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>index page</h1>")
+	r.Use(gee.Logger()) // global midlleware
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>hello gee</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *gee.Context) {
-			c.HTML(http.StatusOK, "<h1>hello, gee</h1>")
-		})
-		v1.GET("/hello", func(c *gee.Context) {
-			// expect /hello?name=haoran
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
+
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *gee.Context) {
 			// expect /hello/haoran
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *gee.Context) {
-			c.JSON(http.StatusOK, gee.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
 	}
 
-	_ = r.Run(":9999")
+	r.Run(":9999")
 }
